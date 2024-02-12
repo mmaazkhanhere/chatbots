3c4a64d6294fafa572503490e7f8ff5d03350dd7
Mian Maaz Ullah Khan <mmaazkhan2107@gmail.com>
2024-02-12 22:30:48 +0500

    fixing issues in echo bot

--- Echo.py
+++ Echo.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title('Echo bot')
 
 if 'e_messages' not in st.session_state:
-    st.session_state.messages = []
+    st.session_state.e_messages = []
     
 for message in st.session_state.e_messages:
     with st.chat_message(message['role']):
@@ -14,11 +14,11 @@ if prompt:
     with st.chat_message('user'):
         st.markdown(prompt)
         
-    st.session_state.e_message.append({'role': 'user', 'content': prompt})
+    st.session_state.e_messages.append({'role': 'user', 'content': prompt})
     
     with st.chat_message('assistant'):
         response = f"Echo: {prompt}"
         st.markdown(response)
 
-    st.session_state.e_message.append({'role':'assistant', 'content': response})    
+    st.session_state.e_messages.append({'role':'assistant', 'content': response})    
 
